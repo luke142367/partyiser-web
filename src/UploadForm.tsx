68a3499b463888898a7414ea54aee608676f3a3d
Luke <lukebenjaminely@gmail.com>
2019-09-13 13:10:46 +0100

    Added transparency toggle

--- src/UploadForm.tsx
+++ src/UploadForm.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './UploadForm.css'
 import { BarLoader } from 'react-spinners';
+import { ChromePicker, ColorChangeHandler, ColorResult } from 'react-color';
 
 interface UploadFormProps {
 }
@@ -10,10 +11,12 @@ const UploadForm: React.FunctionComponent<UploadFormProps> = (props) => {
   const [image, setImage] = React.useState<File>()
   const [responseURL, setResponseURL] = React.useState<string>('')
   const [loading, setLoading] = React.useState<boolean>(false)
+  const [showTrans, setShowTrans] = React.useState<boolean>(false)
+  const [transColour, setTransColour] = React.useState<ColorResult>()
 
   const changeImage = (e:React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0){
-      setImage(e.target.files[0])
+      setImage(e.target.files[0]) 
     }
   }
 
@@ -25,10 +28,22 @@ const UploadForm: React.FunctionComponent<UploadFormProps> = (props) => {
     sendConvertRequest(image)
   }
 
+  const check = (e : React.ChangeEvent<HTMLInputElement>) => {
+    setShowTrans(trans => !trans) 
+  }
+
+  const setColour : ColorChangeHandler = (c) => {
+    setTransColour(c)
+  }
+
   const sendConvertRequest = (image: File) => {
     const data = new FormData()
     data.append('image', image)
-    fetch('https://partyiser-api.herokuapp.com/convert', {
+    let url = 'https://partyiser-api.herokuapp.com/convert'
+    if (showTrans && transColour) {
+      url += '?trans=' + transColour.hex
+    }
+    fetch(url, {
       method: 'POST',
       body: data
     }).then(res => res.json())
@@ -46,13 +61,23 @@ const UploadForm: React.FunctionComponent<UploadFormProps> = (props) => {
             <div className="form-group files">
                 <input type="file" className="form-control" onChange={changeImage}></input>    
             </div>
-              {
-                loading ?
-                <BarLoader loading={loading}  width={400} />
-                :
-                <button className="partyBtn" onClick={convertImage}>Partyise Image!</button>
-              }
           </form>
+          <div className='centreWidth'>
+            <p>Transparency</p>
+            <input type="checkbox" onChange={check} checked={showTrans}/>
+            {showTrans && 
+              <div className='centreColour'>
+                <ChromePicker onChangeComplete={setColour} color={transColour ? transColour.hex : '#00000000'}/>
+              </div>
+            }
+            <br></br>
+            {
+              loading ?
+              <BarLoader loading={loading}  width={400} />
+              :
+              <button className="partyBtn" onClick={convertImage}>Partyise Image!</button>
+            }
+          </div>
           { responseURL && 
           <a href={responseURL + '?download=true'} download='party.gif'>
             <img alt="result" id="resultImg" src={responseURL}/> 
