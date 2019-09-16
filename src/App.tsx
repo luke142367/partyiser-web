addb33590ad305a75796e69c88cc6e11fcf240fe
Luke <lukebenjaminely@gmail.com>
2019-09-16 19:46:42 +0100

    Added eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import './App.css';
-import UploadForm from './UploadForm';
+import * as React from 'react'
+import './App.css'
+import UploadForm from './UploadForm'
 
 function App() {
   return (
     <div className="App">
       <h1 id="title">Partyiser</h1>
-      <UploadForm/>
+      <UploadForm />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/UploadForm.tsx
+++ src/UploadForm.tsx
@@ -1,77 +1,73 @@
-import * as React from 'react';
+import * as React from 'react'
 import './UploadForm.css'
-import { BarLoader } from 'react-spinners';
+import { BarLoader } from 'react-spinners'
 
 interface UploadFormProps {}
 
-const UploadForm: React.FunctionComponent<UploadFormProps> = (props) => {
-
+const UploadForm: React.FunctionComponent<UploadFormProps> = () => {
   const [image, setImage] = React.useState<File>()
   const [responseURL, setResponseURL] = React.useState<string>('')
   const [loading, setLoading] = React.useState<boolean>(false)
   const [trans, setTrans] = React.useState<boolean>(false)
 
   const changeImage = (e:React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0){
-      setImage(e.target.files[0]) 
+    if (e.target.files && e.target.files.length > 0) {
+      setImage(e.target.files[0])
     }
+  }
+
+  const sendConvertRequest = (targetImage: File) => {
+    const data = new FormData()
+    data.append('image', targetImage)
+    let url = 'https://partyiser-api.herokuapp.com/convert'
+    url += `?trans=${trans}`
+    fetch(url, {
+      method: 'POST',
+      body: data,
+    }).then((res) => res.json())
+      .then((res) => {
+        setResponseURL(res.url)
+        setLoading(false)
+      })
   }
 
   const convertImage = (e:React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
     if (!image) return
     setLoading(true)
-    console.log("Uploading image", image.name)
     sendConvertRequest(image)
   }
 
-  const check = (e : React.ChangeEvent<HTMLInputElement>) => {
-    setTrans(trans => !trans) 
-  }
-
-  const sendConvertRequest = (image: File) => {
-    const data = new FormData()
-    data.append('image', image)
-    let url = 'https://partyiser-api.herokuapp.com/convert'
-    url += '?trans=' + trans
-    fetch(url, {
-      method: 'POST',
-      body: data
-    }).then(res => res.json())
-    .then(res => {
-      console.log(res)
-      setResponseURL(res.url)
-      setLoading(false)
-    })
-    console.log("Request sent")
+  const check = () => {
+    setTrans((t) => !t)
   }
 
   return (
-      <div>
-          <form className="form">
-            <div className="form-group files">
-                <input type="file" className="form-control" onChange={changeImage}></input>    
-            </div>
-          </form>
-          <div className='centreWidth'>
-            <p>Transparency</p>
-            <input type="checkbox" onChange={check} checked={trans}/>
-            <br></br>
-            {
-              loading ?
-              <BarLoader loading={loading}  width={400} />
-              :
-              <button className="partyBtn" onClick={convertImage}>Partyise Image!</button>
+    <div>
+      <form className="form">
+        <div className="form-group files">
+          <input type="file" className="form-control" onChange={changeImage} />
+        </div>
+      </form>
+      <div className="centreWidth">
+        <p>Transparency</p>
+        <input type="checkbox" onChange={check} checked={trans} />
+        <br />
+        {
+              loading
+                ? <BarLoader loading={loading} width={400} />
+                : <button type="button" className="partyBtn" onClick={convertImage}>Partyise Image!</button>
             }
-          </div>
-          { responseURL && 
-          <a href={responseURL + '?download=true'} download='party.gif'>
-            <img alt="result" id="resultImg" src={responseURL}/> 
-          </a>
-          }
-
       </div>
-  );
-};
+      { responseURL
+          && (
+          <a href={`${responseURL}?download=true`} download="party.gif">
+            <img alt="result" id="resultImg" src={responseURL} />
+          </a>
+          )}
 
-export default UploadForm;
+    </div>
+  )
+}
+
+export default UploadForm
